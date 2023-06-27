b6ee723f67abae14b3fa054e968f41d7554fca87
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-06-27 13:36:51 -0700

    scaffolded common.js

---
+++ lifetracker-api/tests/common.js
@@ -0,0 +1,24 @@
+const database = require('./db')
+
+const commonBeforeAll = () => {
+
+}
+
+const commonBeforeEach = () => {
+
+}
+
+const commonAfterEach = () => {
+
+}
+
+const commonAfterAll = () => {
+
+}
+
+module.exports = {
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll
+}
