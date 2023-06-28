b7e7c14d8b14b773f741668e6804394a33082c4b
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-06-28 15:11:21 -0700

    added authorization middleware functions and test suite

---
+++ lifetracker-api/middleware/security.js
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview This module contains middleware functions for parsing authentication headers 
+ * and requiring authenticated users in a Node.js Express application.
+ * 
+ * @module middleware/security
+ * @requires config The configuration module with secret key.
+ * @requires tokens The token utility module for token validation.
+ * @requires errors The errors module for throwing Unauthorized errors.
+ * @exports parseAuthHeader
+ * @exports requireAuthenticatedUser
+ */
+const { SECRET_KEY } = require('../config')
+const { validateToken } = require('../utils/tokens')
+const { UnauthorizedError } = require('../utils/errors')
+
+/**
+ * Middleware function to parse the authorization header.
+ * Extracts the token from the authorization header, validates the token and stores the user
+ * in the response locals. If no token is present, it does not store a user.
+ * @param {Object} request The Express request object.
+ * @param {Object} response The Express response object.
+ * @param {Function} next The next middleware function.
+ * @returns {Function} The next middleware function.
+ */
+const parseAuthHeader = (request, response, next) => {
+    const token = request.headers.authorization?.split(' ')[1]
+    if (token) {
+        const user = validateToken(token, SECRET_KEY)
+        response.locals.user = (user)? user : undefined
+    } else {
+        response.locals.user = undefined
+    }
+    return next()
+}
+
+/**
+ * Middleware function to validate the presence of authenticated user.
+ * Throws an UnauthorizedError if no valid user is present in the response locals.
+ * @param {Object} request The Express request object.
+ * @param {Object} response The Express response object.
+ * @param {Function} next The next middleware function.
+ * @returns {Function} The next middleware function.
+ * @throws {UnauthorizedError} If no valid user is present in the response locals.
+ */
+const requireAuthenticatedUser = (request, response, next) => {
+    if (!response.locals.user) {
+        throw new UnauthorizedError()
+    }
+    return next()
+}
+
+module.exports = {
+    parseAuthHeader,
+    requireAuthenticatedUser
+}
---
+++ lifetracker-api/middleware/security.test.js
@@ -0,0 +1,78 @@
+/**
+ * @fileoverview Test file for security middleware functions. This test suite aims to ensure that 
+ * the 'parseAuthHeader' and 'requireAuthenticatedUser' functions are working as expected.
+ *
+ * @module middleware/security.test
+ * @requires jwt JSON Web Token library.
+ * @requires security The security middleware module.
+ * @requires utils/errors The error utility module.
+ * @requires config The configuration module.
+ *
+ * @description
+ * A collection of tests for the 'security' module.
+ * It includes tests for the following functions:
+ * 1. @testsuite 'parseAuthHeader': 
+ *      - Ensures it extracts the user from a valid JWT.
+ *      - Ensures it does not store a user when no valid JWT exists.
+ *      - Ensures it does not store a user when an invalid JWT exists.
+ * 2. @testsuite 'requireAuthenticatedUser': 
+ *      - Throws an 'UnauthorizedError' when no valid user is present.
+ *      - Does not throw an error when a valid user is present.
+ */
+const { parseAuthHeader, requireAuthenticatedUser } = require('./security')
+const { UnauthorizedError } = require('../utils/errors')
+const jwt = require('jsonwebtoken')
+const { SECRET_KEY } = require('../config')
+
+const mockPayload = { username: 'testuser', password: 'testpass' }
+const mockToken = jwt.sign(mockPayload, SECRET_KEY)
+
+describe('parseAuthHeader() middleware', () => {
+    it('should extract the user from a valid JWT', () => {
+        const mockRequest = { headers: { authorization: `Bearer ${mockToken}` } }
+        const mockResponse = { locals: {} }
+
+        parseAuthHeader(mockRequest, mockResponse, () => {})
+
+        expect(mockResponse.locals.user).toMatchObject(mockPayload)
+    })
+
+    it('should not store a user when no valid JWT exists', () => {
+        const mockRequest = { headers: {} }
+        const mockResponse = { locals: {} }
+
+        parseAuthHeader(mockRequest, mockResponse, () => {})
+
+        expect(mockResponse.locals.user).toBeUndefined()
+    })
+
+    it('should not store a user when an in valid JWT exists', () => {
+        const mockRequest = { headers: { authorization: `Bearer invalid` } }
+        const mockResponse = { locals: {} }
+
+        parseAuthHeader(mockRequest, mockResponse, () => {})
+
+        expect(mockResponse.locals.user).toBeUndefined()
+    })
+})
+
+describe('requireAuthenticatedUser() middleware', () => {
+    it('throws an `UnauthorizedError when no valid user is present', () => {
+        const mockRequest = {}
+        const mockResponse = { locals: {} }
+
+        expect(() => {
+            requireAuthenticatedUser(mockRequest, mockResponse, () => {})
+        }).toThrow(UnauthorizedError)
+    })
+
+    it('does not throw an error when a valid user is present', () => {
+        const mockRequest = {}
+        const mockResponse = { locals: { user: mockPayload } }
+        const next = jest.fn()
+
+        requireAuthenticatedUser(mockRequest, mockResponse, next)
+
+        expect(next).toHaveBeenCalled()
+    })
+})
