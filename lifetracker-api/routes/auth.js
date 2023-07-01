ce089d2d29071fc2d3bd4d2c135a320e2602463a
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-06-30 23:39:25 -0700

    made concise and prettier

--- lifetracker-api/routes/auth.js
+++ lifetracker-api/routes/auth.js
@@ -1,43 +1,67 @@
+/**
+ * @fileoverview Express router for authentication operations like login, registration and fetching current user data.
+ * @module routes/auth
+ * @requires express - Express framework for handling HTTP requests and responses.
+ * @requires ../models/users - The User model for database interaction.
+ * @requires ../config - Configuration module containing the secret key for token creation.
+ * @requires ../utils/tokens - Token utilities module for generating user tokens.
+ * @requires ../middleware/security - Middleware for handling security-related operations such as authentication.
+ */
+
 const express = require('express')
 const User = require('../models/users')
 const { SECRET_KEY } = require('../config')
 const { createToken } = require('../utils/tokens')
-const { requireAuthenticatedUser } = require('../middleware/security')
+const requireAuth = require('../middleware/security').requireAuthenticatedUser
 
 const authRouter = express.Router()
 
-authRouter.post('/login', async (request, response) => {
+/**
+ * Handles user login, creates a token for the authenticated user.
+ * @name login
+ * @route {POST} /login
+ * @authentication This route requires no authentication.
+ * @bodyparam {string} email, password - User credentials.
+ */
+authRouter.post('/login', async ({ body: { email, password } }, res) => {
     try {
-        const { email, password } = request.body
         const user = await User.login({ email, password })
         const token = createToken({ email: user.email }, SECRET_KEY)
-
-        response.status(200).json({ token: token, user: user })
-    } catch (error) {
-        response.status(error.status).json({ error: error.message })
+        res.status(200).json({ token, user })
+    } catch ({ status, message }) {
+        res.status(status).json({ error: message })
     }
 })
 
-authRouter.post('/register', async (request, response) => {
+/**
+ * Registers a new user, creates a token for the registered user.
+ * @name register
+ * @route {POST} /register
+ * @authentication This route requires no authentication.
+ * @bodyparam {Object} credentials - User credentials.
+ */
+authRouter.post('/register', async ({ body: credentials }, res) => {
     try {
-        const credentials = request.body
         const user = await User.register(credentials)
         const token = createToken({ email: user.email }, SECRET_KEY)
-
-        response.status(201).json({ token: token, user: user })
-    } catch (error) {
-        response.status(error.status).json({ error: error.message })
+        res.status(201).json({ token, user })
+    } catch ({ status, message }) {
+        res.status(status).json({ error: message })
     }
 })
 
-authRouter.get('/me', requireAuthenticatedUser, async (request, response) => {
+/**
+ * Fetches the authenticated user's data.
+ * @name me
+ * @route {GET} /me
+ * @authentication This route requires authenticated users.
+ */
+authRouter.get('/me', requireAuth, async ({ locals: { user: { email } } }, res) => {
     try {
-        const { email } = response.locals.user
         const user = await User.fetchUserByEmail(email)
-
-        response.status(200).json({ user: user })
-    } catch (error) {
-        response.status(error.status).json({ error: error.message })
+        res.status(200).json({ user })
+    } catch ({ status, message }) {
+        res.status(status).json({ error: message })
     }
 })
 
