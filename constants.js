1e9abee11f027c7204a47740d27faba7e2a40926
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-07-02 22:12:51 -0700

    added api url variables for dev and prod

---
+++ constants.js
@@ -0,0 +1,6 @@
+export const PRODUCTION_API_BASE_URL = 'http://localhost:3001'
+export const DEVELOPMENT_API_BASE_URL = 'http://localhost:3001'
+export const API_BASE_URL =
+    process.env.NODE_ENV === 'production'
+        ? PRODUCTION_API_BASE_URL
+        : DEVELOPMENT_API_BASE_URL
