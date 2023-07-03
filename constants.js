c61b7dc196838c4e0f5a3eb1ee67caca9d2521c7
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-07-02 22:15:27 -0700

    added docstring bc why not

--- constants.js
+++ constants.js
@@ -1,3 +1,11 @@
+/**
+ * @fileoverview This file exports constants that are used to configure the base URL for API calls.
+ * @module constants
+ * @exports PRODUCTION_API_BASE_URL - The base URL of the API when the app is running in a production environment.
+ * @exports DEVELOPMENT_API_BASE_URL - The base URL of the API when the app is running in a development environment.
+ * @exports API_BASE_URL - The base URL that should be used for API calls, determined based on the current environment.
+ */
+
 export const PRODUCTION_API_BASE_URL = 'http://localhost:3001'
 export const DEVELOPMENT_API_BASE_URL = 'http://localhost:3001'
 export const API_BASE_URL =
